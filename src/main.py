fa1104d2857285fcf809303fab5e5449583df6ae
Serch Roa <serch.roa.rivera@gmail.com>
2023-02-16 12:40:06 -0600

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+"""
+Autor:          Sergio Roa Rivera/ furbis99 / serch_roa_
+App Name:       Alarm Clock
+Date:           16-Feb-2023
+Last Update:    
+Description:    Project of an alarm clock made with python.
+"""
